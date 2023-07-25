71f9ddd87190e22370cac721ac13864b9631f9ff
Vonney <davonnejv@gmail.com>
2023-07-25 18:06:07 -0500

    adding useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
+import { useState, useEffect} from 'react'
 import SignUpForm from './components/SignUpForm'
 import Authenticate from './components/Authenticate'
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    console.log("The page has mounted")
+  }, [])
 
 
   return (
