0e4e09d097b21038f7328eb3a3558f88066d2a49
Vonney <davonnejv@gmail.com>
2023-07-25 20:17:22 -0500

    adding h1 and google font

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ function App() {
 
   return (
     <>
-     <SignUpForm token={token} setToken={setToken} />
-     <Authenticate token={token} setToken={setToken}/>
+      <h1>React Forms<br/>
+      Block 27 Workshop</h1>
+
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken}/>
     </>
   )
 }
