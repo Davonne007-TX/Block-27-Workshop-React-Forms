410de1f3ce659a02432bed20e3e11b6017a08433
Vonney <davonnejv@gmail.com>
2023-07-25 19:28:59 -0500

    adding useState to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import { useState } from 'react'
 import SignUpForm from './components/SignUpForm'
 import Authenticate from './components/Authenticate'
 
 function App() {
+  const [token, setToken] = useState(null);
 
   return (
     <>
