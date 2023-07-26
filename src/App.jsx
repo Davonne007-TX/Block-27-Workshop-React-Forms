e962b651ff8a732ab8702fd6870ed73b6546eb00
Vonney <davonnejv@gmail.com>
2023-07-25 19:40:38 -0500

    switching signup and authenticate components

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <>
-     <Authenticate token={token} setToken={setToken}/>
      <SignUpForm token={token} setToken={setToken} />
+     <Authenticate token={token} setToken={setToken}/>
     </>
   )
 }
