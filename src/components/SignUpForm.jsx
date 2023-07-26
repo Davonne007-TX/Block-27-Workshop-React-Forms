414abc81feea795b9f444809d0cef46abcab18af
Vonney <davonnejv@gmail.com>
2023-07-25 19:03:02 -0500

    adding useState to SigUpForm.jsx instead

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useState, useEffect} from 'react'
 
 function SignUpForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        console.log("The page has mounted")
+      }, [])
+
   return (
   <h2>Sign Up</h2>
   )
