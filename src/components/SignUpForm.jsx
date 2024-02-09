6ff37634b0eff0bf05028dde176a5abe55ec6e34
Vonney <davonnejv@gmail.com>
2024-02-08 21:49:53 -0600

    adding mobile responsivness

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,53 +1,55 @@
-import React from 'react'
-import { useState, useEffect} from 'react'
-import './SignUpForm.css' 
+import React from "react";
+import { useState, useEffect } from "react";
+import "./SignUpForm.css";
 
 function SignUpForm({ setToken }) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        console.log("The page has mounted")
-      }, [])
+  useEffect(() => {
+    console.log("The page has mounted");
+  }, []);
 
-    async function handleSubmit(event) {
-      event.preventDefault();
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-      if(username.length === 0 || password.length === 0) {
-        setError("You must enter a username and password, please try again");
-        return;
-      } else if (username.length < 4 || password < 4 ) {
-        setError("Username and Password needs to be more than 4 characters");
-        return;
-     }
+    if (username.length === 0 || password.length === 0) {
+      setError("You must enter a username and password, please try again");
+      return;
+    } else if (username.length < 4 || password < 4) {
+      setError("Username and Password needs to be more than 4 characters");
+      return;
+    }
 
-      try {
-        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({username, password}),
-        })
+          body: JSON.stringify({ username, password }),
+        }
+      );
 
-        const result = await response.json();
-        console.log(result);
-        setToken(result.token)
-        setError(null)
+      const result = await response.json();
+      console.log(result);
+      setToken(result.token);
+      setError(null);
 
-        alert("Open up console log to see if sign up was successful")
-
-      } catch (error) {
-          setError(error.message);
-      }
-    } //last curly to handelSubmit
-
-    //to reset the form
-    function resetForm () {
-      setUsername("");
-      setPassword("");
+      alert("Open up console log to see if sign up was successful");
+    } catch (error) {
+      setError(error.message);
     }
+  } //last curly to handelSubmit
+
+  //to reset the form
+  function resetForm() {
+    setUsername("");
+    setPassword("");
+  }
 
   return (
     <>
@@ -56,40 +58,39 @@ function SignUpForm({ setToken }) {
         {error && <p>{error}</p>}
 
         <form onSubmit={handleSubmit}>
-           <label>
-             Username: {""}
-             <input
-              value={username} 
-              placeholder="Enter Username" 
-              onChange={(e) => setUsername(e.target.value)} />      
-           </label>
-
           <label>
-             Password: {""} 
-             <input
-              type="password" 
-              placeholder="Enter Password" 
-              value={password} onChange={(e) => setPassword(e.target.value)} 
-              className="passwordInput"/>
+            Username: {""}
+            <input
+              value={username}
+              placeholder="Enter Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </label>
 
-           <div className="myButtons">
-             <button className="submitButton">Submit</button>
-             <input 
-             type="reset" 
-             value="Reset Form" 
-             className="resetForm" onClick={resetForm} />
-           </div>
+          <label>
+            Password: {""}
+            <input
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {/* // className="passwordInput"  */}
+          </label>
+
+          <div className="myButtons">
+            <button className="submitButton">Submit</button>
+            <input
+              type="reset"
+              value="Reset Form"
+              className="resetForm"
+              onClick={resetForm}
+            />
+          </div>
         </form>
       </div>
-   </>
-  )
+    </>
+  );
 }
 
-export default SignUpForm
-
-
-
-
-
-
+export default SignUpForm;
